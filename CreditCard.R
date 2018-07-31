fb087718cfbedef4579865576227827b689d63e6
Utsav Gupta <34511793+utsavgpt5@users.noreply.github.com>
2018-07-31 10:34:42 +0530

    Update CreditCard.R

--- CreditCard.R
+++ CreditCard.R
@@ -7,7 +7,7 @@ library(ggplot2)
 library(caTools)
 library(data.table)
 #loading the data
-setwd("C:/Users/Utsav/Desktop/CreditCard_Proj")
+#setwd("C:/Users/Utsav/Desktop/CreditCard_Proj")
 #setwd("")
 cust_data<-fread("cc_dbase.csv")
 cust_data_backup<-cust_data
@@ -20,7 +20,7 @@ cust_data_subset<-cust_data[,-c("custid","age","birthmonth","ed","employ","incom
                                 ,"commutebus","commuterail","commutepublic","commutebike","commutewalk","commutenonmotor"
                                 ,"telecommute","polview","polparty","polcontrib","vote","cardtenure","card2tenure","bfast","lnlongmon","lntollmon","lnlongten","lntollten","lnequipmon"
                                 ,"lnequipten","lncardmon","lncardten","lnwiremon","lnwireten"
-                          )]
+)]
 #separating columns according to factors and numeric ones
 cust_cat<-cust_data_subset[,-c("debtinc","reside","pets","creddebt","othdebt"
                                ,"cars","carditems","card2items","card2spent","cardspent",
@@ -44,7 +44,7 @@ str(cust_data_sub)
 summary(cust_data_sub)
 
 #checking for NAs
-na.values<-View(cust_data_sub[!complete.cases(cust_data_sub)])
+View(cust_data_sub[!complete.cases(cust_data_sub)])
 
 #reordering columns(not working)
 col_names<-names(cust_data_subset)
@@ -57,31 +57,29 @@ cust_data_sub[cust_data_sub$region=="1" & is.na(cust_data_sub$townsize),"townsiz
 
 mean_reg_twn_5<-as.numeric(cust_data%>%filter(region=="5")%>%select(townsize)%>%summarize(mn=ceiling(median(townsize,na.rm=T))))
 cust_data_sub[cust_data_sub$region=="5" & is.na(cust_data_sub$townsize),"townsize"]<-as.factor(mean_reg_twn_5)
-View(cust_data_sub[complete.cases(cust_data_sub)])
+View(cust_data_sub[!complete.cases(cust_data_sub)])
 
 #combining card 1 spent and card 2 spent
 cust_data_sub$total_cardspent<-cust_data_sub$cardspent+cust_data_sub$card2spent
 head(cust_data_sub[,c("cardspent","card2spent","total_cardspent")])
+#removing incomplete rows
 cust_data_sub<-cust_data_sub[complete.cases(cust_data_sub)]
-#plotting for correlation
-temp.cor<-cor(cust_data_sub[,c(85,1:22)])
-corrplot(temp.cor,method = "pie")
 
 #outliers
 p<-ggplot(data=cust_data_sub)
 p1<-p+geom_point(aes(x=debtinc,y=total_cardspent))
 p1
 
-#3SD
+#3SD-removing outliers
 cust_cat2<-cust_data_sub[,-c("debtinc","reside","pets","creddebt","othdebt"
-                               ,"cars","carditems","card2items","card2spent","cardspent",
-                               "tenure","longmon","longten","tollmon","tollten","equipmon","equipten",
-                               "cardmon","cardten","wiremon","wireten","hourstv")]
+                             ,"cars","carditems","card2items","card2spent","cardspent",
+                             "tenure","longmon","longten","tollmon","tollten","equipmon","equipten",
+                             "cardmon","cardten","wiremon","wireten","hourstv","total_cardspent")]
 
 cust_num2<-cust_data_sub[,c("debtinc","reside","pets","creddebt","othdebt"
-                              ,"cars","carditems","card2items","card2spent","cardspent",
-                              "tenure","longmon","longten","tollmon","tollten","equipmon","equipten",
-                              "cardmon","cardten","wiremon","wireten","hourstv")]
+                            ,"cars","carditems","card2items","card2spent","cardspent",
+                            "tenure","longmon","longten","tollmon","tollten","equipmon","equipten",
+                            "cardmon","cardten","wiremon","wireten","hourstv","total_cardspent")]
 
 cust_num2<-as.data.frame(apply(cust_num2,2,function(x)
 {med<-median(x)
@@ -96,6 +94,23 @@ sapply(x,function(y)
 })
 
 }))
-
-View(cust_num2[complete.cases(cust_num2)])
+#combining again
 final_sub<-cbind(cust_num2,cust_cat2)
+View(final_sub)
+
+#plotting for correlation
+temp.cor<-cor(final_sub[,c(1:23)])
+corrplot(temp.cor,method = "color")
+#backup
+final_sub1<-final_sub
+
+#removing more columns
+final_sub<-final_sub[,c("total_cardspent", "creddebt","othdebt","carditems","card2items",
+                        "wiremon","wireten","tollmon","tollten","equipmon","equipten","cars",
+                        "agecat","gender","marital","retire","inccat","jobcat","empcat","edcat",
+                        "spousedcat","region" ,"addresscat","carown","cartype","carcatvalue",
+                        "reason","churn","card","card2","cardtenurecat","card2tenurecat","internet",
+                        "ownpda","ownvcr","owndvd","owncd","owntv"
+                        )]
+temp.cor<-cor(final_sub[,c(1:12)])
+corrplot(temp.cor,method = "color")
